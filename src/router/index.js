61e7c5b5e331fb21e06e55fa2c61f66bbb39905d
Carlos Daroya <daroyac@gmail.com>
2018-10-22 01:39:07 +0800

    Connected web3 0.20.5 with vueJS

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '@/components/Dashboard'
 import Signup from '@/components/Signup'
+import CasinoDapp from '@/components/casino-dapp'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/signup',
       name: 'signup',
       component: Signup
+    },
+    {
+      path: '/casino',
+      name: 'casino-dapp',
+      component: CasinoDapp
     }
   ]
 })
