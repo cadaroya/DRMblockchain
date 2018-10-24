4d1f54c046bc5ba74fb05a54be479c0918d992f2
Jules Segismundo <julessegismundo@gmail.com>
2018-10-25 00:59:02 +0800

    Created LicenseManager

--- contracts/ProductManager.sol
+++ contracts/ProductManager.sol
@@ -11,75 +11,82 @@ contract ProductManager {
         bool renewable;
     }
 
-    struct License {
-        uint256 productId;
-        uint256 attributes;
-        uint256 issuedTime;
-        uint256 expirationTime;
-        address affiliate;
-    }
+    uint256[] public productIds; 
 
-    Product[] public products; 
-    License[] public licenses;
 
     mapping (address => uint256) public vendorProductCount;
-    mapping (address => uint256) public ownerLicenseCount;
-    mapping (uint256 => address) public productToVendor;
-    mapping (uint256 => address) public licenseToOwner;
+    
 
-    event ProductRegistered(uint256 productId, uint256 productIdentifier);
+    mapping (uint256 => Product) public products;
+    mapping (uint256 => address) public productIdToVendor;
 
-    /*
-    event LicenseIssued(
-        address indexed vendor,
-        address indexed buyer,
-        uint256 licenseId,
-        uint256 productId,
-        uint256 attributes,
-        uint256 issuedTime,
-        uint256 expirationTime,
-        address affiliate
-    );
-    */
 
-    modifier licenseNotExpired(uint256 _licenseId) {
-        require(licenses[_licenseId].expirationTime >= now || licenses[_licenseId].expirationTime == 0, "License has expired");
-        _;
-    }
+    event ProductRegistered(uint256 productId, uint256 price, uint256 available, uint256 supply, uint256 sold, uint256 interval);
+    
+
 
     modifier onlyVendor(uint256 _id) {
         require(msg.sender == productToVendor[_id], "Sender is not the vendor of this product");
         _;
     }
 
+    function priceOf(uint256 _productId) public view returns (uint256) {
+        return products[_productId].price;
+    }
+
+    function availableAmountOf(uint256 _productId) public view returns (uint256) {
+        return products[_productId].available;
+    }
+
+    function supplyOf(uint256 _productId) public view returns (uint256) {
+        return products[_productId].supply;
+    }
+
+    function amountSold(uint256 _productId) public view returns (uint256) {
+        return products[_productId].sold;
+    }
+
+    function intervalOf(uint256 _productId) public view returns (uint256) {
+        return products[_productId].interval;
+    }
+
+    function isRenewable(uint256 _productId) public view returns (bool) {
+        return products[_productId].renewable;
+    }
+
+
+    function _productExists(uint256 _productId) internal view returns (bool) {
+        return (products[_productId].id != 0);
+    } 
+
+    function _productDoesNotExist(uint256 _productId) internal view returns (bool) {
+        return (products[_productId].id == 0);
+    } 
+
     function _registerProduct(uint256 _id, uint256 _price, uint256 _available, 
             uint256 _supply, uint256 _sold, uint256 _interval, 
             bool _renewable) internal {
+        require(_productDoesNotExist(_id), "Product does not exist");
         Product storage product = Product(_id, _price, _available, _supply, _sold, _interval, _renewable);
-        uint256 id = products.push(product) - 1;
-        productToVendor[id] = msg.sender;
+        productIds.push(_id);
+        products[_id] = product;
+        productIdToVendor[_id] = msg.sender;
         vendorProductCount[msg.sender]++;
-        emit ProductRegistered(id, _id);
+        emit ProductRegistered(_id, _price, _available, _supply, _sold, _interval, _renewable);
     }
 
-    /*
-    function _createLicense(uint256 _productId, uint256 _attributes, uint256 _issuedTime, 
-            uint256 _expirationTime, address _affiliate) internal {
+    
+    
+    
 
-        License storage license = License(_productId, _attributes, now, _expirationTime, _affiliate);
-        uint256 id = licenses.push(license) - 1;
-    }
-    */
-
-    /*
+    
     function _buyProduct(uint256 _productId, address _buyer) {
         Product product = products[_productId];
         product.supply--;
         product.available--;
         product.sold++;
-
     }
-    */
+    
 
 
   
