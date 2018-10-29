021830bc2ab38d4f95b407da25c5c3867aa89cac
cadaroya <daroyac@gmail.com>
2018-10-29 17:39:11 +0800

    merge Push

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,8 @@ var LicenseManager = artifacts.require("./LicenseManager.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Users);
-<<<<<<< HEAD
-  deployer.deploy(ProductManager);
-  deployer.deploy(LicenseManager);
-=======
   deployer.deploy(ProductManager).then( () =>{
     return deployer.deploy(LicenseManager);
   });
   
->>>>>>> e178e17c1c43eb80d63fda5503ec618f8dc1728d
 };
