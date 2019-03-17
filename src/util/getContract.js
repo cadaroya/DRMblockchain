35aec6e3006454de352a1365837abc3b0430a060
cadaroya <daroyac@gmail.com>
2019-03-17 21:39:02 +0800

    Done Simulation

--- src/util/getContract.js
+++ src/util/getContract.js
@@ -3,10 +3,10 @@ import JSONInterface from '../../build/contracts/LicenseManager.json'
 
 let getContract = new Promise(function (resolve, reject) {
   let web3 = new Web3(window.web3.currentProvider)
-  // '0x582e81be13caffc69753108296be075c6059c425' LOCAL GANACHE
-  // '0x86feedcd197beb00f3fb3dcdeaee580f7e847b5d' new local ganache
+  // '0xb46e2d50cde22962b5b840107e1b652ad764988e' LOCAL GANACHE
   // '0xd73b1df53f329c7f50c2550a7cd40cbf61ae8c53' ROPSTEN ONLINE
-  let licenseContractInstance = new web3.eth.Contract(JSONInterface.abi, '0x36231dc7a46e182068e835bf5991667866b265fa')
+  // '0x61fab8dc1e3f90436fda5cc9c92c3ffa2c632562' Truffle test instance.address
+  let licenseContractInstance = new web3.eth.Contract(JSONInterface.abi, '0x61fab8dc1e3f90436fda5cc9c92c3ffa2c632562')
   licenseContractInstance.methods.test().call((err, res) => {
     if (!err) {
       console.log(res)
--- test/truffle/Simulation.js
+++ test/truffle/Simulation.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 
-var nUsers = 10;
+var nUsers = 2;
 var nTxns = 2;
 contract('LicenseManager', async accounts => {
     let instance = null;
@@ -92,6 +92,44 @@ contract('LicenseManager', async accounts => {
         console.log("Done");
 
     })
+
+    
+    it("users should be able to verify licenses", async () => {
+        var productId = 1;
+        var result;
+
+        for(var i = 0; i < nUsers; i++){
+            for(var j = 1; j <= nTxns; j++){
+                var productId = ((i+1)*nTxns)%(nUsers*nTxns)+j;
+                result = await instance.verifyLicenseOwnership(accounts[i], productId, {from: accounts[i]});
+                // console.log(result);
+            }
+        }
+
+
+        
+    });
+
+    it("users should be able to transfer licenses to other users", async () => {
+        // 0: 3,4, 1: 1,2
+        // soo... license[0] is product3.
+        // too complicated, and unrealistic. SO. lets just transfer everything an account owns to account 0
+
+
+
+        for(var i = 0; i < nUsers; i++){
+            result = await instance.viewOwnerLicenses(accounts[i]);
+            var length = result.length;
+            
+            for(var j = 0; j < length; j++){
+                var licenseId = result[j]["c"][0];
+                await instance.transfer(accounts[0], licenseId, {from: accounts[i]});
+            }
+
+        }
+        console.log(instance.address);
+
+    });
     
 
 });
